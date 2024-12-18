c2ff424d9d19690da33e4f53d81f618273675a64
Yawson-Enoch <enochgybex25@gmail.com>
2024-12-18 21:27:13 +0000

    feat: initial commit

---
+++ app/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="font-display bg-background text-foreground min-h-dvh">
+    <p>Hello GyBex</p>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,29 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  future: { compatibilityVersion: 4 },
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+
+  css: ['~/app.css'],
+  postcss: {
+    plugins: {
+      '@tailwindcss/postcss': {},
+    },
+  },
+
+  modules: ['@nuxt/fonts'],
+  fonts: {
+    provider: 'google',
+    experimental: {
+      processCSSVariables: true,
+    },
+  },
+
+  /* disable auto-imports */
+  imports: {
+    autoImport: false,
+  },
+  components: {
+    dirs: [],
+  },
+});
